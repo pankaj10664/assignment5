5f2dae1b224e14703593b17d909878932b66a42a
pankaj10664 <pankaj10664@gmail.com>
2018-06-10 17:36:49 +0530

    triangle pattern of stars

---
+++ star.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Ark
+
+{
+public static void main(String args[])
+{
+int i,j;
+int k=1;
+
+for(i=5;i>=k;i--)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+}
+}
